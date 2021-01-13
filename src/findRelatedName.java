a006ff66264d74c3af8f82121b0fcad890741316
梁嘉慧 <liangjiahui_x@163.com>
2021-01-13 20:45:20 +0800

    2 spot related

---
+++ src/findRelatedName.java
@@ -0,0 +1,30 @@
+public class findRelatedName {
+    public static void main(String[] args) {
+        String[] oldN={"m","-","Base","Instance"};
+        String[] newN={"m","-","Instance"};
+        String[] context={"m_BaseInstPanel","m_AttPanel","m_AttSummaryPanel","setBaseInstanceFromFileQ","setBaseInstanceFromDBQ","setBaseInstanceFromURLQ"};
+
+    }
+    //分析重构活动
+    public static void renameAnalyzer(){
+
+    }
+    //与标识符代表的实体有关系的所有实体的标识符
+    public static String[] researchEnginne(){
+        String[] res={};
+        return res;
+
+    }
+    //过滤res集合
+    public static String[] filter(){
+        String[] res={};
+        return res;
+
+    }
+    //generate Segmentation pairs
+    public String[] generateSub(String[] identifier){
+        String[] subSeq={};
+        return subSeq;
+    }
+
+}
